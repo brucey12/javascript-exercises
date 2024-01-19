e0e88eab37547fefb8bb96eb2ce8d64ead06e05e
Nolan Bruce <93457002+brucey12@users.noreply.github.com>
2024-01-18 18:08:49 -0800

    Edit function arguments
    Edited function parameters to include a rest operator. Allows user
    to more easily be grouped into a new array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+// 3 dots with the args paramater is called a rest operator
+// tells JS to gather remaining parameters into an array with that name
+const removeFromArray = function (array, ...args) {
+  // define new array
+  const newArray = [];
+  // use a forEach loop to go through the array
+  array.forEach((item) => {
+    // push elements into new array
+    // unless they're included in the function arguments
+    // as those should be removed
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  // return the new array
+  return newArray;
 
+  // a somewhat simpler but more complicated way to do it
+  //   return array.filter((val) => !args.includes(val));
 };
 
 // Do not edit below this line
